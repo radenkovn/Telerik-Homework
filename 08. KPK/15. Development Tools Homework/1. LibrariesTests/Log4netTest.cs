816f67346f8c2ff6cffe3f8d71f7964a6dbfde59
radenkovn <radenkovn@yahoo.com>
2015-08-21 22:20:58 +0300

    Added the 15th Homework

---
+++ 08. KPK/15. Development Tools Homework/1. LibrariesTests/Log4netTest.cs
@@ -0,0 +1,16 @@
+﻿namespace LibrariesTests
+{
+    using System;
+    using System.Linq;
+    using log4net;
+
+    class Log4netTest
+    {
+        static void Main()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            ILog log = LogManager.GetLogger(typeof(string));
+            log.Error("This is an example of a log message");
+        }
+    }
+}
---
+++ 08. KPK/15. Development Tools Homework/2. TemplateTests/TemplateTest.cs
@@ -0,0 +1,41 @@
+﻿
+
+public class Test
+{
+		public int TestProperty0 { get; set; }
+		public int TestProperty1 { get; set; }
+		public int TestProperty2 { get; set; }
+		public int TestProperty3 { get; set; }
+		public int TestProperty4 { get; set; }
+		public int TestProperty5 { get; set; }
+		public int TestProperty6 { get; set; }
+		public int TestProperty7 { get; set; }
+		public int TestProperty8 { get; set; }
+		public int TestProperty9 { get; set; }
+		public int TestProperty10 { get; set; }
+		public int TestProperty11 { get; set; }
+		public int TestProperty12 { get; set; }
+		public int TestProperty13 { get; set; }
+		public int TestProperty14 { get; set; }
+		public int TestProperty15 { get; set; }
+		public int TestProperty16 { get; set; }
+		public int TestProperty17 { get; set; }
+		public int TestProperty18 { get; set; }
+		public int TestProperty19 { get; set; }
+	}
+
+public class Test1
+{
+	public Test CurrentTest { get; set; }
+
+}
+public class Test2
+{
+	public Test CurrentTest { get; set; }
+
+}
+public class Test3
+{
+	public Test CurrentTest { get; set; }
+
+}
