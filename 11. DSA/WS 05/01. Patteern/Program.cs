47afc78448da0847cc2a06204ef500e05b495eda
radenkovn <radenkovn@yahoo.com>
2015-12-04 08:22:04 +0200

    ws 5

---
+++ 11. DSA/WS 05/01. Patteern/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Patteern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            int n = text.Length;
+            text = text + text;
+
+            int[] fl = new int[n + 1];
+            fl[0] = -1;
+            fl[1] = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                int j = fl[i];
+
+                while (j >= 0 && text[i] != text[j])
+                {
+                    j = fl[j];
+                }
+
+                fl[i + 1] = j + 1;
+            }
+
+            int matched = 0;
+
+            for (int i = 1; true; i++)
+            {
+
+                while (matched >= 0 && text[i] != text[matched])
+                {
+                    matched = fl[matched];
+
+                }
+                matched++;
+
+                if (matched == n)
+                {
+                    Console.WriteLine(text.Substring(0, i - n + 1));
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ 11. DSA/WS 05/04. Rabbits/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Rabbits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            var inputAnswers = line.Split(' ').Select(int.Parse).ToList();
+
+            inputAnswers.RemoveAt(inputAnswers.Count - 1);
+
+            var answer = MinRabitsCount(inputAnswers);
+            Console.WriteLine(answer);
+        }
+
+        public static int MinRabitsCount(IEnumerable<int> answers)
+        {
+            var groups = new Dictionary<int, int>();
+            foreach (var answer in answers)
+            {
+                if (!groups.ContainsKey(answer + 1))
+                {
+                    groups.Add(answer + 1, 0);
+                }
+
+                groups[answer + 1]++;
+            }
+
+            var rabbits = 0;
+
+            foreach (var group in groups)
+            {
+                var size = group.Key;
+                var rabbitsInGroup = group.Value;
+                var groupsCount = (int)Math.Ceiling(rabbitsInGroup / (decimal)size);
+                rabbits += groupsCount * size;
+            }
+
+            return rabbits;
+
+        }
+    }
+}
